28c81cb7b8dc7c82a590a2224dc9bd2038e56993
전혜지 <gpfp7@naver.com>
2021-04-26 18:48:06 +0900

    -

--- jblog/sql/jblog.sql
+++ jblog/sql/jblog.sql
@@ -1,21 +1,23 @@
--- jblog 계정 생성: system 계정에서 수행
--- create user jblog identified by jblog;
--- grant resource, connect to jblog;
 
--- 테이블 삭제
+
+-- jblog ���� ����: system �������� ����
+create user jblog identified by jblog;
+grant resource, connect to jblog;
+
+-- ���̺� ����
 drop table comments;
 drop table post;
 drop table category;
 drop table blog;
 drop table users;
 
--- 시퀀스 삭제
+-- ������ ����
 drop sequence seq_users_no;
 drop sequence seq_category_no;
 drop sequence seq_post_no;
 drop sequence seq_comments_no;
 
--- 시퀀스 생성
+-- ������ ����
 CREATE SEQUENCE seq_users_no
 INCREMENT BY 1
 START WITH 1 
@@ -37,7 +39,7 @@ INCREMENT BY 1
 START WITH 1 
 NOCACHE ;
 
--- 테이블 생성
+-- ���̺� ����
 CREATE TABLE users (
   userNo    NUMBER,
   id	    VARCHAR2(50) 	NOT NULL Unique,
@@ -88,3 +90,138 @@ CREATE TABLE comments (
   CONSTRAINT c_comment_fk FOREIGN KEY (postNo)
   REFERENCES post(postNo)
 );
+
+
+INSERT INTO users(userNo,id,userName,password,joinDate) values(seq_users_no.nextval,'wlgud30','������','wlgud12',sysdate);
+INSERT INTO category(cateNo,userNo,cateName,description,regDate) values(seq_category_no.nextval,33,'a','aa',sysdate);
+INSERT INTO post(postNo,cateNo,postTitle,postContent,regDate) values(seq_post_no.nextval,3,'post2','post content2',sysdate);
+INSERT INTO comments(cmtNo,postNo,userNo,cmtContent,regDate) values(seq_comments_no.nextval,1,34,'cmt222222',sysdate);
+SELECT 
+				cateNo,userNo,cateName,description,regDate
+			FROM
+				category
+			WHERE
+				userNo = 725
+			ORDER BY
+				regDate;
+
+UPDATE
+				blog
+			SET
+				blogTitle='�������� ��α� �Դϴ�.',logoFile=''
+			WHERE
+				userNo = 33;
+
+delete from users;
+delete from blog;
+delete from category;
+
+commit;
+
+select * from users;
+select * from blog;
+select * from category;
+select * from post;
+select * from comments;
+
+select c.cateNo,cateName,nvl(cnt,0) as cnt,description 
+from category c left join (select count(*) cnt ,cateNo  from post where cateNo is not null group by cateNo) b
+on c.cateNo = b.cateNo ;
+select nvl(c.count,0) from (select count(*) count  from post where cateNo = 4 group by cateNo) c;
+select count(*) count  from post where cateNo = 4 group by cateNo;
+SELECT 
+				c.cateNo,cateName,nvl(cnt,0) as cnt,description,c.regDate
+			FROM 
+				category c 
+			LEFT JOIN 
+				(
+				SELECT
+					count(*) cnt ,cateNo  
+				FROM 
+					post 
+				WHERE 
+					cateNo is not null 
+				GROUP BY
+					cateNo
+				) b
+			ON 
+				c.cateNo = b.cateNo
+            WHERE
+                c.userNo = 725
+			ORDER BY
+				c.regDate;
+
+select count(*) as cnt,cateNo from post group by cateNo;
+
+SELECT B.userNo,blogTitle,logoFile,C.userNo,cateName
+FROM
+	blog b , category c
+WHERE
+    b.userNo = c.userNo
+AND
+    b.userNo = 725;
+
+select * from user_sys_privs;
+
+grant connect,resource to c##jblog;
+SELECT seq_users_no.currval FROM dual ;
+commit;
+
+alter user c##jblog quota unlimited on blog;
+
+SELECT * FROM POST;
+
+
+    
+SELECT
+    *
+FROM
+    post
+WHERE
+    postNo = '1';
+SELECT
+				postTitle,regDate
+			FROM
+				post
+			WHERE
+				cateNo = 3;
+
+SELECT 
+				COUNT(*)
+			FROM
+				users
+			WHERE
+				id = 'wjsgpwl7';
+
+
+SELECT 
+    *
+FROM
+    category a left join post b
+on
+    a.cateNo  = b.cateNO 
+where
+    userNo = 725;
+    
+SELECT 
+    *
+FROM
+    category a
+WHERE
+    userNo = 725;
+    
+SELECT 
+				cmtNo,postNo,cmt.userNo,cmtContent,regDate,userName
+			FROM
+				comments cmt, users u
+			WHERE
+                cmt.userNo = u.userNo
+            AND
+				postNo = 1;
+                
+SELECT 
+    cateNo
+FROM
+    post
+WHERE
+    postNo = 1;
